72100d37ed0cb45a85953c5ca516b440a2ea5c5c
JorgeGuzman <jorguzman100@gmail.com>
2025-12-16 12:55:51 +0200

    19 itself

---
+++ itself.js
@@ -0,0 +1,27 @@
+const sender = {
+    name: "Tom",
+    address: "123 Main St"
+}
+
+const deliveryPackage = {
+    contents: "porridge",
+    sender: sender,
+}
+
+const trackingInfo = {
+    sender: sender,
+    status: "in transit"
+}
+
+console.log("sender: ", sender);
+console.log("deliveryPackage: ", deliveryPackage);
+console.log("trackingInfo: ", trackingInfo);
+
+sender.name = 'Jerry';
+
+console.log("sender: ", sender);
+console.log("deliveryPackage: ", deliveryPackage);
+console.log("trackingInfo: ", trackingInfo);
+
+console.log("package.sender.name: ", deliveryPackage.sender.name);
+console.log("trackingInfo.sender.name: ", trackingInfo.sender.name);
