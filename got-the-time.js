f67418065877f5681e63840cc0a6fd1ecc0e7063
JorgeGuzman <jorguzman100@gmail.com>
2025-12-16 14:49:48 +0200

    23 got-the-time

---
+++ got-the-time.js
@@ -0,0 +1,23 @@
+const printPrettyDate = (date) => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months = ["January", "February", "March", "April", "May", "June",
+                  "July", "August", "September", "October", "November", "December"];
+
+  const day = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const dayOfMonth = date.getDate();
+  const year = date.getFullYear();
+
+  // Hora ya formateada como "03:50:37 PM"
+  const time = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  return `Today is ${day}, ${month} ${dayOfMonth}, ${year}, and the time is ${time}.`;
+};
+
+
+console.log(printPrettyDate(new Date()));
