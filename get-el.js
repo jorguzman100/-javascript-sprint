20ff39781b24e38edede3cdbd1a73ca262d2fed3
JorgeGuzman <jorguzman100@gmail.com>
2025-12-31 17:27:51 +0200

    31 get-el

---
+++ get-el.js
@@ -0,0 +1,23 @@
+// const getElementsByTag = (tagType) => {
+//     return document.querySelectorAll(tagType);
+// }
+const getElementsByTag = (tagType) => {
+    return document.getElementsByTagName(tagType);
+}
+const getElementsByClassName = (className) => {
+    return document.getElementsByClassName(className);
+}
+const getElementById = (id) => {
+    return document.getElementById(id);
+}
+const getElementsByAttribute = (attribute, value) => {
+    return value ? document.querySelectorAll(`[${attribute}="${value}"]`) :
+    document.querySelectorAll(`[${attribute}]`) ;
+}
+
+// console.log("a: ", getElementsByTag('a'));
+// console.log("item ", getElementsByClassName('item'));
+// console.log("submit-button: ", getElementById('submit-button'));
+// console.log("type: ", getElementsByAttribute('type'));
+// console.log("data-category: ", getElementsByAttribute('data-category'));
+// console.log("[data-category, a]: ", getElementsByAttribute('data-category', 'a'));
