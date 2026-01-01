919dd10f995fd3a253acaf5d1d3ce161d89e8997
JorgeGuzman <jorguzman100@gmail.com>
2026-01-01 23:06:23 +0200

    34 toogle-class

---
+++ toggle-class/toggle-class.js
@@ -0,0 +1,48 @@
+// State
+
+
+// Initialize
+
+// The content
+const content = document.createElement('div');
+const p = document.createElement('p');
+content.id = 'content'
+p.textContent = 'code';
+content.append(p);
+document.body.append(content);
+
+// The controls
+const controls = document.createElement('div');
+controls.classList.add('controls');
+for(let i = 0; i < 4; i++) {
+    const button = document.createElement('BUTTON');
+    if (i === 0) {
+        button.id = 'bold';
+        button.textContent = 'B';
+    }
+    if (i === 1) {
+        button.id = 'italic';
+        button.textContent = 'I';
+    }
+    if (i === 2) {
+        button.id = 'underline';
+        button.textContent = 'U';
+    }
+    if (i === 3) {
+        button.id = 'highlight';
+        button.textContent = 'Highlight';
+    }
+    controls.append(button);
+}
+document.body.append(controls);
+
+
+// Render
+
+// Listen to update state
+document.body.addEventListener('click', (event) => {
+    const button = event.target.tagName === 'BUTTON' ? event.target.id : null;
+    if (button) {
+        p.classList.toggle(`${button}`);
+    }
+});
